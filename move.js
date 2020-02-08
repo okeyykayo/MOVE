2989a33ec70f72ca9d3a37fcf33dd87a91751160
okeyykayo <markivanloveyou@gmail.com>
2020-02-08 16:38:09 +0800

    Mark Ivan C. Balote

---
+++ move.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $('.right').click(function(){
+        $('#box').css(
+        { left: "+=5px"}
+        );
+    });
+});
+$(document).ready(function(){
+    $('.left').click(function(){
+        $('#box').css(
+        { left: "-=5px"}
+        );
+    });
+});
+$(document).ready(function(){
+    $('.down').click(function(){
+        $('#box').css(
+        { top: "+=5px"}
+        );
+    });
+});
+$(document).ready(function(){
+    $('.up').click(function(){
+        $('#box').css(
+        { top: "-=5px"}
+        );
+    });
+});
+
+$(document).keydown(function(e){
+//console.log(e.which);
+    var key=e.which;
+    /** left **/
+    var left_keys=[65,37];
+    if (left_keys.includes(key)){
+        $('.left').click();
+    }
+    var up_keys=[87,38];
+    if (up_keys.includes(key)){
+        $('.up').click();
+    }
+     var right_keys=[68,39];
+    if (right_keys.includes(key)){
+        $('.right').click();
+    }
+     var down_keys=[83,40];
+    if (down_keys.includes(key)){
+        $('.down').click();
+    }
+});
